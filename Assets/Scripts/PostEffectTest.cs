809796a65dec038a1c3ad9e8d020c6753e94400b
LitLov3 <912778422@qq.com>
2018-11-29 21:15:38 +0800

    some test code

---
+++ Assets/Scripts/PostEffectTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PostEffectTest : MonoBehaviour {
+
+    public Shader postEffectShader;
+    private Material postMat;
+    private CommandBuffer cmd;
+    private Camera cam;
+    private RenderTexture stencilCopy;
+
+    void Start () {
+        //stencilCopy = Shader.PropertyToID("stencilCopy");
+        stencilCopy = new RenderTexture(Screen.width, Screen.height, 24);
+        postMat = new Material(postEffectShader);
+        cam = GetComponent<Camera>();
+        cmd = new CommandBuffer();
+        cmd.name = "stencil copy";
+
+        cam.AddCommandBuffer(CameraEvent.AfterForwardOpaque, cmd);
+	}
+
+    private void OnPreRender()
+    {
+        if (cmd == null)
+            return;
+
+        cmd.Clear();             
+        cmd.Blit(BuiltinRenderTextureType.CurrentActive, stencilCopy);
+    }
+
+    [ImageEffectOpaque]
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if(postMat != null)
+        {
+            //Graphics.SetRenderTarget(stencilCopy);
+            Graphics.Blit(src, dst, postMat);
+        }
+        else
+        {
+            Graphics.Blit(src, dst);
+        }
+    }
+}
