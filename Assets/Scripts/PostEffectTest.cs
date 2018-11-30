fa6e4601d37adae9b5ab51cedf7acc5a693e68f2
LitLov3 <912778422@qq.com>
2018-11-30 16:36:36 +0800

    simple and easy after check renderdoc carefully QQ

--- Assets/Scripts/PostEffectTest.cs
+++ Assets/Scripts/PostEffectTest.cs
@@ -7,56 +7,26 @@ public class PostEffectTest : MonoBehaviour {
 
     public Shader postEffectShader;
     private Material postMat;
-    private CommandBuffer cmd;
+    private CommandBuffer cb_stencilCopy;
+    private CommandBuffer cb_finalBlit;
     private Camera cam;
     private RenderTexture stencilCopy;
-    private List<StencilMaskObject> maskList = new List<StencilMaskObject>();
 
-    void Start () {
-
+    void Start()
+    {
         postMat = new Material(postEffectShader);
         stencilCopy = new RenderTexture(Screen.width, Screen.height, 24);
-        stencilCopy.name = "StencilCopy";
-
-        maskList = new List<StencilMaskObject>(FindObjectsOfType<StencilMaskObject>());
-
-        cam = GetComponent<Camera>();
-        cmd = new CommandBuffer();
-        cmd.name = "stencil copy";
-
-        cam.AddCommandBuffer(CameraEvent.AfterForwardOpaque, cmd);
-	}
-
-    private void OnPreRender()
-    {
-        if (cmd == null)
-            return;
-
-        cmd.Clear();
-        //cmd.SetRenderTarget(BuiltinRenderTextureType.CurrentActive, stencilCopy.depthBuffer);
-        //cmd.SetRenderTarget(stencilCopy);
-        //cmd.Blit(BuiltinRenderTextureType.CurrentActive, stencilCopy);
-        cmd.SetRenderTarget(stencilCopy);
-        foreach (var obj in maskList)
-        {
-            cmd.DrawRenderer(obj.GetComponent<Renderer>(), obj.GetComponent<Renderer>().sharedMaterial, 0, 0);
-        }
+        stencilCopy.name = "stencilCopy";
     }
 
+
     private void OnRenderImage(RenderTexture src, RenderTexture dst)
-    { 
-        if (postMat != null)
-        {         
-            var temp = RenderTexture.GetTemporary(Screen.width, Screen.height, 24);
-            Graphics.Blit(src, temp);
-            Graphics.SetRenderTarget(temp.colorBuffer, stencilCopy.depthBuffer);
-            Graphics.Blit(src, postMat);
-            Graphics.Blit(temp, dst);
-            RenderTexture.ReleaseTemporary(temp);
-        }
-        else
-        {
-            Graphics.Blit(src, dst);
-        }
+    {
+        var buffer = RenderTexture.GetTemporary(Screen.width, Screen.height);
+        Graphics.Blit(src, buffer);
+        Graphics.SetRenderTarget(buffer.colorBuffer, src.depthBuffer);
+        Graphics.Blit(src, postMat);
+        Graphics.Blit(buffer, dst);
+        RenderTexture.ReleaseTemporary(buffer);
     }
 }
